f1026a2e7f0dfc416246c82f66636b1339762573
andreeabrindea <andreeabrindea61@gmail.com>
2025-05-23 20:21:37 +0300

    refactor: change types for packageQuantity and unit + add setters and getters

--- src/main/java/com/PriceComparatorBackend/PriceComparatorBackend/model/Product.java
+++ src/main/java/com/PriceComparatorBackend/PriceComparatorBackend/model/Product.java
@@ -12,13 +12,15 @@ public class Product {
     String productName;
     String productCategory;
     String brand;
-    int packageQuantity;
-    int unit;
-    double price;
+    Double packageQuantity;
+    String unit;
+    Double price;
     String currency;
+    String storeName;
+    String date;
 
-    public Product(String productId, String productName, String productCategory, String brand, int packageQuantity,
-            int unit, double price, String currency) {
+    public Product(String productId, String productName, String productCategory, String brand, Double packageQuantity,
+            String unit, Double price, String currency, String storeName, String date) {
         this.productId = productId;
         this.productName = productName;
         this.productCategory = productCategory;
@@ -27,5 +29,87 @@ public class Product {
         this.unit = unit;
         this.price = price;
         this.currency = currency;
+        this.storeName = storeName;
+        this.date = date;
     }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public String getProductCategory() {
+        return productCategory;
+    }
+
+    public void setProductCategory(String productCategory) {
+        this.productCategory = productCategory;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public Double getPackageQuantity() {
+        return packageQuantity;
+    }
+
+    public void setPackageQuantity(Double packageQuantity) {
+        this.packageQuantity = packageQuantity;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    public String getCurrency() {
+        return currency;
+    }
+
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+    public String getStoreName() {
+        return storeName;
+    }
+
+    public void setStoreName(String storeName) {
+        this.storeName = storeName;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }    
 }
