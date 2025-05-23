a420d59fef674b4998c8370dade806e1461c8504
andreeabrindea <andreeabrindea61@gmail.com>
2025-05-23 20:04:43 +0300

    refactor: remove already moved code

--- src/main/java/com/PriceComparatorBackend/PriceComparatorBackend/PriceComparatorBackendApplication.java
+++ src/main/java/com/PriceComparatorBackend/PriceComparatorBackend/PriceComparatorBackendApplication.java
@@ -1,26 +1,12 @@
 package com.PriceComparatorBackend.PriceComparatorBackend;
-import java.io.BufferedReader;
-import java.io.FileReader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import com.PriceComparatorBackend.PriceComparatorBackend.Utils.Utils;
 
 @SpringBootApplication
 public class PriceComparatorBackendApplication {
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(PriceComparatorBackendApplication.class, args);
-        Utils.createDataset();
-        try (BufferedReader br = new BufferedReader(new FileReader("src/main/java/com/PriceComparatorBackend/PriceComparatorBackend/dataset.csv"))) {
-            String line;
-            while ((line = br.readLine()) != null) {
-                System.out.println(line);
-            }
-        }
-        catch(Exception e)
-        {   
-            System.out.println(e.getMessage());
-        }
     }
 }
