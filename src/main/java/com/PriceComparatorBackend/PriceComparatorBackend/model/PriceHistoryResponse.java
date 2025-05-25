1bbfb61b8c589d2948540fd3a39bc6aa79f7a9da
andreeabrindea <andreeabrindea61@gmail.com>
2025-05-25 20:36:32 +0300

    refactor: added price history points to PriceHistoryResponse + setters && getters

--- src/main/java/com/PriceComparatorBackend/PriceComparatorBackend/model/PriceHistoryResponse.java
+++ src/main/java/com/PriceComparatorBackend/PriceComparatorBackend/model/PriceHistoryResponse.java
@@ -1,14 +1,21 @@
 package com.PriceComparatorBackend.PriceComparatorBackend.model;
 
+import java.util.List;
+
 public class PriceHistoryResponse {
     private String productId;
+    private String productName;
+    private List<PriceHistoryPoint> priceHistory;
     private PriceStatistics priceStatistics;
 
     public PriceHistoryResponse() {
     }
 
-    public PriceHistoryResponse(String productId, PriceStatistics priceStatistics) {
+    public PriceHistoryResponse(String productId, String productName, List<PriceHistoryPoint> priceHistory,
+            PriceStatistics priceStatistics) {
         this.productId = productId;
+        this.productName = productName;
+        this.priceHistory = priceHistory;
         this.priceStatistics = priceStatistics;
     }
 
@@ -18,6 +25,22 @@ public class PriceHistoryResponse {
 
     public void setProductId(String productId) {
         this.productId = productId;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public List<PriceHistoryPoint> getPriceHistory() {
+        return priceHistory;
+    }
+
+    public void setPriceHistory(List<PriceHistoryPoint> priceHistory) {
+        this.priceHistory = priceHistory;
     }
 
     public PriceStatistics getPriceStatistics() {
