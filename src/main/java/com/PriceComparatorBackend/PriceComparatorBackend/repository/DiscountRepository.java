59ef740961ef59ba869844a5acc94f85b8e14481
andreeabrindea <andreeabrindea61@gmail.com>
2025-05-25 11:59:21 +0300

    refactor: use CSVReader instead of BufferedReader to read file
    to avoid human error

--- src/main/java/com/PriceComparatorBackend/PriceComparatorBackend/repository/DiscountRepository.java
+++ src/main/java/com/PriceComparatorBackend/PriceComparatorBackend/repository/DiscountRepository.java
@@ -2,14 +2,18 @@ package com.PriceComparatorBackend.PriceComparatorBackend.repository;
 
 import com.PriceComparatorBackend.PriceComparatorBackend.model.Discount;
 import com.PriceComparatorBackend.PriceComparatorBackend.model.Product;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 import com.PriceComparatorBackend.PriceComparatorBackend.Utils.Utils;
 
-import java.io.BufferedReader;
 import java.io.FileReader;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.stereotype.Repository;
 
@@ -26,35 +30,39 @@ public class DiscountRepository {
             System.out.println("failed to create dataset");
             return null;
         }
-        try (BufferedReader br = new BufferedReader(new FileReader(
-                "src/main/java/com/PriceComparatorBackend/PriceComparatorBackend/discountsDataset.csv"))) {
-            String line;
-            int lineCount = 0;
-            List<Discount> discounts = new ArrayList<Discount>();
+
+        List<Discount> discounts = new ArrayList<>();
+        try (CSVReader reader = new CSVReaderBuilder(
+                new FileReader("src/main/java/com/PriceComparatorBackend/PriceComparatorBackend/discountsDataset.csv"))
+                .withCSVParser(new CSVParserBuilder().withSeparator(';').build())
+                .build()) {
+
             DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-
-            while ((line = br.readLine()) != null) {
-                if (lineCount > 0) {
-                    String[] entries = line.split(";");
-                    String productId = entries[0];
-                    LocalDate fromDate = LocalDate.parse(entries[6], formatter);
-                    LocalDate toDate = LocalDate.parse(entries[7], formatter);
-                    int discountPercent = Integer.parseInt(entries[8]);
-                    Product product = productRepository.findProductById(productId);
-
-                    if (product != null) {
-                        discounts.add(new Discount(product, fromDate, toDate, discountPercent));
-                    } else {
-                        System.out.println("Product not found for ID: " + productId);
-                    }
-                }
-                lineCount++;
+            String[] headers = reader.readNext();
+            Map<String, Integer> headerMap = new HashMap<>();
+            for (int i = 0; i < headers.length; i++) {
+                headerMap.put(headers[i].trim(), i);
             }
 
-            return discounts;
+            String[] line;
+            while ((line = reader.readNext()) != null) {
+                String productId = line[headerMap.get("product_id")];
+                Product product = productRepository.findProductById(productId);
+                if (product != null) {
+                    product.setDate(LocalDate.parse(line[headerMap.get("date")], formatter));
+                    discounts.add(new Discount(
+                            product,
+                            LocalDate.parse(line[headerMap.get("from_date")], formatter),
+                            LocalDate.parse(line[headerMap.get("to_date")], formatter),
+                            Integer.parseInt(line[headerMap.get("percentage_of_discount")])));
+                } else {
+                    System.out.println(String.format("Product not found for ID %s. Price remains unknown.", productId));
+                }
+            }
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        return null;
+
+        return discounts;
     }
 }
--- src/main/java/com/PriceComparatorBackend/PriceComparatorBackend/repository/ProductRepository.java
+++ src/main/java/com/PriceComparatorBackend/PriceComparatorBackend/repository/ProductRepository.java
@@ -1,51 +1,67 @@
 package com.PriceComparatorBackend.PriceComparatorBackend.repository;
 
-import java.io.BufferedReader;
 import java.io.FileReader;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import com.PriceComparatorBackend.PriceComparatorBackend.model.Product;
+import com.opencsv.CSVParserBuilder;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 import com.PriceComparatorBackend.PriceComparatorBackend.Utils.Utils;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class ProductRepository {
 
-    public List<Product> getAllProducts(){
-        try
-        {
+    public List<Product> getAllProducts() {
+        try {
             Utils.createDataset("dataset", "data");
-        }
-        catch (Exception exception)
-        {
+        } catch (Exception exception) {
             System.out.println(exception.getMessage());
             return null;
         }
-        try (BufferedReader br = new BufferedReader(new FileReader("src/main/java/com/PriceComparatorBackend/PriceComparatorBackend/dataset.csv"))) {
-            String line;
-            int lineCount = 0;
-            List<Product> products = new ArrayList<Product>();
-            while ((line = br.readLine()) != null) {
-                if (lineCount > 0)
-                {
-                    String[] entries = line.split(";");
-                    products.add(new Product(entries[0], entries[1], entries[2], entries[3], Double.parseDouble(entries[4]), entries[5], Double.parseDouble(entries[6]), entries[7], entries[8], entries[9]));
-                }
-                lineCount++;
+
+        List<Product> products = new ArrayList<>();
+        try (CSVReader reader = new CSVReaderBuilder(
+                new FileReader("src/main/java/com/PriceComparatorBackend/PriceComparatorBackend/dataset.csv"))
+                .withCSVParser(new CSVParserBuilder().withSeparator(';').build())
+                .build()) {
+
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            String[] headers = reader.readNext();
+
+            Map<String, Integer> headerMap = new HashMap<>();
+            for (int i = 0; i < headers.length; i++) {
+                headerMap.put(headers[i].trim(), i);
             }
 
-            return products;
-        }
-        catch(Exception e)
-        {   
+            String[] line;
+            while ((line = reader.readNext()) != null) {
+                products.add(new Product(
+                        line[headerMap.get("product_id")],
+                        line[headerMap.get("product_name")],
+                        line[headerMap.get("product_category")],
+                        line[headerMap.get("brand")],
+                        Double.parseDouble(line[headerMap.get("package_quantity")]),
+                        line[headerMap.get("package_unit")],
+                        Double.parseDouble(line[headerMap.get("price")]),
+                        line[headerMap.get("currency")],
+                        line[headerMap.get("store_name")],
+                        LocalDate.parse(line[headerMap.get("date")], formatter)));
+            }
+
+        } catch (Exception e) {
             System.out.println(e.getMessage());
         }
-        return null;
+        return products;
     }
 
-    public Product findProductById(String id)
-    {
+    public Product findProductById(String id) {
         List<Product> products = getAllProducts();
         return products.stream().filter(p -> p.getProductId().equalsIgnoreCase(id)).findAny().orElse(null);
     }
