45f393682d87687c20d5b69224f542a140bfe0fc
andreeabrindea <andreeabrindea61@gmail.com>
2025-05-25 20:34:01 +0300

    refactor: remove unused fields
    initially I added to many fields that are not needed

--- src/main/java/com/PriceComparatorBackend/PriceComparatorBackend/model/ProductFilter.java
+++ src/main/java/com/PriceComparatorBackend/PriceComparatorBackend/model/ProductFilter.java
@@ -1,44 +1,20 @@
 package com.PriceComparatorBackend.PriceComparatorBackend.model;
 
-import java.time.LocalDate;
 
 public class ProductFilter {
-    private String productId;
-    private String productName;
     private String storeName;
     private String productCategory;
     private String brand;
-    private LocalDate startDate;
-    private LocalDate endDate;
+
 
     public ProductFilter() {
     }
 
-    public ProductFilter(String productId, String productName, String storeName, String productCategory,
-            String brand, LocalDate startDate, LocalDate endDate) {
-        this.productId = productId;
-        this.productName = productName;
+    public ProductFilter(String storeName, String productCategory, String brand) {
+
         this.storeName = storeName;
         this.productCategory = productCategory;
         this.brand = brand;
-        this.startDate = startDate;
-        this.endDate = endDate;
-    }
-
-    public String getProductId() {
-        return productId;
-    }
-
-    public void setProductId(String productId) {
-        this.productId = productId;
-    }
-
-    public String getProductName() {
-        return productName;
-    }
-
-    public void setProductName(String productName) {
-        this.productName = productName;
     }
 
     public String getStoreName() {
@@ -63,21 +39,5 @@ public class ProductFilter {
 
     public void setBrand(String brand) {
         this.brand = brand;
-    }
-
-    public LocalDate getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(LocalDate startDate) {
-        this.startDate = startDate;
-    }
-
-    public LocalDate getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(LocalDate endDate) {
-        this.endDate = endDate;
     }
 }
